f1c28f098e87b8be56a338f486c71a5829561f33
Daniel Diewald <diewald.d02@htlwienwest.at>
2021-06-04 21:19:53 +0200

    v.0.1

---
+++ script.js
@@ -0,0 +1,19 @@
+let x = 0;
+let y = 0;
+
+function countsimulationtrue(){
+    x =+ 1;
+
+}
+function countsimulationfalse(){
+    y =+ 1;
+}
+function github(){
+    window.location.href = "https://github.com/DanielDiewald/arewereak";
+}
+function htlwienwest(){
+    window.location.href = "https://www.htlwienwest.at/#";
+}
+function crusadersworld(){
+    window.location.href = "http://htl.losko.at/crusadersworld/";
+}
